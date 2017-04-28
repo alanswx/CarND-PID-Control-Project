fa6558ce46afcd98d305e093b5191aac2c845065
Alan Steremberg <alan.steremberg@gmail.com>
2017-04-27 18:08:35 -0700

    first change

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,35 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  this.Kp=Kp;
+  this.Ki=Ki;
+  this.Kd=Kd;
 }
 
 void PID::UpdateError(double cte) {
+  p_error = cte;
+  i_error += cte;
+  d_error = cte - p_error;
+#if 0
+def run(robot, tau_p, tau_d, tau_i, n=100, speed=1.0):
+    x_trajectory = []
+    y_trajectory = []
+    prev_cte = robot.y
+    int_cte = 0
+    for i in range(n):
+        cte = robot.y
+        diff_cte = cte - prev_cte
+        prev_cte = cte
+        int_cte += cte
+        steer = -tau_p * cte - tau_d * diff_cte - tau_i * int_cte
+        robot.move(steer, speed)
+        x_trajectory.append(robot.x)
+        y_trajectory.append(robot.y)
+    return x_trajectory, y_trajectory
+#endif
 }
 
 double PID::TotalError() {
+        return -Kp* p_error - Kd* d_error - Ki * i_error;
 }
 
